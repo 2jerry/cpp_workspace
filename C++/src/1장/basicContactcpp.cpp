d897db3c376ceec9c22992ea64964d9aaac881a2
2jerry <wodml9412@gmail.com>
2018-01-15 17:15:40 +0900

    chapter 2 add

--- C++/src/1장/basicContactcpp.cpp
+++ C++/src/1장/basicContactcpp.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
 #include <fstream>
+#include <string>
+
+#define OWNER 1
+#define CONTACT 0
 
 using namespace std;
+
 
 struct OwnerInfo {
 	char name[10];
@@ -17,10 +22,27 @@ struct ContactInfo {
 } contacts[100];
 int contactNumber = 0;
 
-void writeFile(ContactInfo contact)
+
+void ownerWriteFile(OwnerInfo owner1);
+void contactWriteFile(ContactInfo contact);
+void inputOwnerInfo();
+void outputOwnerInfo();
+void addContact();
+void outputContactList();
+void runOwnerMenu();
+void runContactMenu();
+void runMainMenu();
+int removeContactByIndex(int index);
+void editOwnerInfo();
+int removeContactByIndex(int index);
+void printContactInfoByIndex(int index);
+
+
+void ownerWriteFile(OwnerInfo owner1)
 {
-	ContactInfo con = contact;
-	ofstream ofs("info.txt",ios::app);
+	OwnerInfo con = owner1;
+	
+	fstream ofs("o_info.txt",ios::app);
 	ofs <<"이름 : " <<con.name << endl;
 	ofs << "연락처 : " <<con.phoneNumber << endl;
 	ofs << "이메일 : "<<con.email << endl;
@@ -28,7 +50,20 @@ void writeFile(ContactInfo contact)
 	
 
 	ofs.close();
-	}
+}
+void contactWriteFile(ContactInfo contact)
+{
+	ContactInfo con = contact;
+
+	fstream ofs("c_info.txt", ios::app);
+	ofs << "이름 : " << con.name << endl;
+	ofs << "연락처 : " << con.phoneNumber << endl;
+	ofs << "이메일 : " << con.email << endl;
+	ofs << "주소 : " << con.address << endl;
+
+
+	ofs.close();
+}
 
 void inputOwnerInfo()
 {
@@ -44,6 +79,8 @@ void inputOwnerInfo()
 	cout << "소유자 주소 : ";
 	cin >> owner.address;
 
+	ownerWriteFile(owner);
+
 	cout << "입력이 완료되었습니다." << endl;
 }
 
@@ -53,6 +90,14 @@ void outputOwnerInfo()
 	cout << "소유자 전화번호: " << owner.phoneNumber << endl;
 	cout << "소유자 이메일: " << owner.email << endl;
 	cout << "소유자 주소: " << owner.address << endl;
+	
+	/*
+	fstream ifs("o_info.txt",ios::in);
+	string str;
+	ifs >> str ;
+	cout << str << endl;
+	ifs.close();
+	*/
 }
 
 void addContact()
@@ -69,20 +114,25 @@ void addContact()
 	cout << "연락처 주소 : ";
 	cin >> contacts[contactNumber].address;
 
-	writeFile(contacts[contactNumber]);
+	contactWriteFile(contacts[contactNumber]);
 
 	contactNumber++;
 	cout << "입력이 완료되었습니다." << endl;
 
-	
 }
 
 void outputContactList()
 {
+	/*fstream ifs("c_info.txt", ios::in);
+	string str;
+	ifs >> str;
+	cout << str << endl;
+	ifs.close();*/
+	
 	int i;
 	for (i = 0; i < contactNumber; i++)
 		cout << i << " : " << contacts[i].name << endl;
-
+		
 }
 
 void runOwnerMenu()
@@ -92,7 +142,8 @@ void runOwnerMenu()
 	do {
 		cout << "1. 소유자 정보 입력" << endl;
 		cout << "2. 소유자 정보 조회" << endl;
-		cout << "3. 이전 화면" << endl;
+		cout << "3. 소유자 정보 수정" << endl;
+		cout << "4. 이전 화면" << endl;
 		cin >> menuNum;
 
 		switch (menuNum)
@@ -104,22 +155,26 @@ void runOwnerMenu()
 			outputOwnerInfo();
 			break;
 		case 3:
-			cout << "프로그램을 종료합니다." << endl;
+			editOwnerInfo();
+			break;
+		case 4:
 			break;
 		default:
 			cout << "다시 입력하세요." << endl;
 		}
-	} while (menuNum != 3);
+	} while (menuNum != 4);
 }
 
 void runContactMenu()
 {
-	int menuNum;
+	int menuNum,index,res;
 
 	do {
 		cout << "1. 연락처 추가 입력" << endl;
 		cout << "2. 연락처 목록 조회" << endl;
-		cout << "3. 이전 화면" << endl;
+		cout << "3. 연락처 목록 삭제" << endl;
+		cout << "4. 연락처 상세 조회" << endl;
+		cout << "5. 이전 화면" << endl;
 		cin >> menuNum;
 
 		switch (menuNum)
@@ -131,14 +186,29 @@ void runContactMenu()
 			outputContactList();
 			break;
 		case 3:
-			cout << "프로그램을 종료합니다." << endl;
+			cout << "삭제할 연락처 : ";
+			cin >> index;
+			res = removeContactByIndex(index);
+			if (res)
+				cout << "정상 삭제" << endl;
+			else
+				cout << "비정상 삭제" << endl;
+			break;
+		case 4:
+			cout << "조회할 연락처 : ";
+			cin >> index;
+			printContactInfoByIndex(index);
+			
+		case 5:
+			cout << "이전화면" << endl;
 			break;
 		default:
 			cout << "다시 입력하세요." << endl;
 
 		}
-	} while (menuNum != 3);
+	} while (menuNum != 5);
 }
+
 void runMainMenu()
 {
 	int menuNum;
@@ -165,6 +235,63 @@ void runMainMenu()
 
 		}
 	} while (menuNum != 3);
+}
+
+void editOwnerInfo()// 소유자 정보 수정
+{
+	int inputNum;
+
+	cout << "1. 소유자 이름 수정 " << endl;
+	cout << "2. 소유자 전화번호 수정 " << endl;
+	cout << "3. 소유자 이메일 수정 " << endl;
+	cout << "4. 소유자 주소 수정 " << endl;
+	cout << "5. 이전메뉴 " << endl;
+
+	cin >> inputNum;
+
+	switch (inputNum)
+	{
+	case 1:
+		cin >> owner.name;
+		break;
+	case 2:
+		cin >> owner.phoneNumber;
+		break;
+	case 3:
+		cin >> owner.email;
+		break;
+	case 4:
+		cin >> owner.address;
+		break;
+	default:
+		cout << "수정 아무것도 안됌" << endl;
+		break;
+	}
+}
+
+int removeContactByIndex(int index) //  연락처 제거
+{
+	if ((index >= contactNumber) || (index < 0))
+		return 0;
+
+	int i;
+	for (i = index + 1; i < contactNumber; i++)
+		*(contacts + i - 1) = *(contacts + i); // 뒤의 배열의 요소를 덮어 씌운다,
+
+	contactNumber--;
+
+	return 1;
+}
+
+void printContactInfoByIndex(int index)
+{
+	cout << index << "의 index를 가진 연락처 조회" << endl;
+	cout << "연락처 이름 : " << contacts[index].name << endl;
+	cout << "연락처 전화번호 : " << contacts[index].phoneNumber << endl;
+	cout << "연락처 이메일 : " << contacts[index].email << endl;
+	cout << "연락처 주소 : " << contacts[index].address << endl;
+
+
 }
 
 int main()
