3a6c40de8ec8316d281e89fa4680faa74ba39109
2jerry <wodml9412@gmail.com>
2018-01-22 16:53:45 +0900

    chap 4 add

---
+++ C++/src/4장/FunctionConcept1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+class Chulsoo
+{
+public :
+	void Eat();
+};
+
+void Chulsoo::Eat()
+{
+	cout << "Ã¶¼ö´Â ¸Ô´Â´Ù" << endl;
+}
+
+int main()
+{
+	Chulsoo chulsoo;
+	chulsoo.Eat();
+
+}
